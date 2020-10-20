3f8b8f84ebe36f37565028353672740039f7b162
trizen <trizen@protonmail.com>
2020-10-20 23:45:25 +0300

    - Fixed the "403: Forbidden" errors for newly uploaded videos on unpopular channels.
    When "dashManifestUrl" or "sc=yes" is detected, we now use the fallback extraction method (youtube-dl or invidious instances).

--- lib/WWW/YoutubeViewer.pm
+++ lib/WWW/YoutubeViewer.pm
@@ -901,6 +901,11 @@ sub _extract_streaming_urls {
 
         my $streamingData = $json->{streamingData};
 
+        if (defined $streamingData->{dashManifestUrl}) {
+            say STDERR ":: Contains DASH manifest URL" if $self->get_debug;
+            return;
+        }
+
         if (exists $streamingData->{adaptiveFormats}) {
             push @results, @{$streamingData->{adaptiveFormats}};
         }
@@ -911,6 +916,11 @@ sub _extract_streaming_urls {
     }
 
     $self->_check_streaming_urls($videoID, \@results);
+
+    if (grep { $_->{url} =~ /\bsc=yes\b/ } @results) {
+        say STDERR ":: Contains SC = yes" if $self->get_debug;
+        return;
+    }
 
     # Keep only streams with contentLength > 0.
     @results = grep { $_->{itag} == 22 or (exists($_->{contentLength}) and $_->{contentLength} > 0) } @results;
